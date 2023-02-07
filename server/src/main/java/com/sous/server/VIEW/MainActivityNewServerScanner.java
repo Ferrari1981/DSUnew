5c384f4fd75818e855831104ced027ade3f63c11
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2023-02-07 15:31:33 +0300

    DSU----07,02,2023----15,31---ска Удаление разрешили локальное GPS--.zip

--- server/src/main/java/com/sous/server/VIEW/MainActivityNewServerScanner.java
+++ server/src/main/java/com/sous/server/VIEW/MainActivityNewServerScanner.java
@@ -290,7 +290,7 @@ public class MainActivityNewServerScanner extends AppCompatActivity  {
                     Manifest.permission.WRITE_EXTERNAL_STORAGE,
                     Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,
+                  /*  Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,*/
                     Manifest.permission.BLUETOOTH_SCAN,
                     Manifest.permission.BLUETOOTH_CONNECT,
                     Manifest.permission.BLUETOOTH_PRIVILEGED,
@@ -300,7 +300,7 @@ public class MainActivityNewServerScanner extends AppCompatActivity  {
             String[] PERMISSIONS_LOCATION = {
                     Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,
+                /*    Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,*/
                     Manifest.permission.BLUETOOTH_SCAN,
                     Manifest.permission.BLUETOOTH_CONNECT,
                     Manifest.permission.BLUETOOTH_PRIVILEGED,
